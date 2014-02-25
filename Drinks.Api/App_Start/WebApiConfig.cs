863b376cd7c10ccc7a8291a3861a819a60ba0e53
Levi Botelho <levi.botelho@satimo.fr>
2014-02-25 17:40:44 +0100

    Removed api segment from route

--- Drinks.Api/App_Start/WebApiConfig.cs
+++ Drinks.Api/App_Start/WebApiConfig.cs
@@ -19,7 +19,7 @@ namespace Drinks.Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
